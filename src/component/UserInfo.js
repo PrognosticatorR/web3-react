d7b6d1f8edee4ed495105410407c2a576e62e3e7
deveshrawat <devsrawt@gmail.com>
2020-12-14 14:29:56 +0530

    update user info

--- src/component/UserInfo.js
+++ src/component/UserInfo.js
@@ -1,24 +1,30 @@
 import React from "react";
 import { Typography } from "antd";
+import { Avatar } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 const { Text } = Typography;
 
 const UserInfo = ({ accounts, balance }) => {
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
         paddingLeft: "20px",
         lineHeight: "2rem",
       }}
     >
-      <Text style={{ fontSize: "16px", color: "#1C90FF" }}>
-        Eth Address: {accounts[0]}
-      </Text>
-      <Text style={{ fontSize: "16px", color: "#1C90FF" }}>
-        Balance: {balance} WEENUS
-      </Text>
+      <Avatar.Group style={{ flexDirection: "column", alignItems: "center" }}>
+        <Avatar
+          shape="square"
+          size="large"
+          icon={<UserOutlined color="#001628" />}
+        />
+        <Text style={{ fontSize: "16px" }}>
+          Eth Address: <strong>{accounts[0]}</strong>{" "}
+        </Text>
+        <Text style={{ fontSize: "16px", color: "#001628" }}>
+          BALANCE: <b>{balance}</b> WEENUS Tokens
+        </Text>
+      </Avatar.Group>
     </div>
   );
 };
